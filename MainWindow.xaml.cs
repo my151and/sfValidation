71dd8ae7fdad30e41efd7fff8c396c8153e44341
my151and <33822968+my151and@users.noreply.github.com>
2018-03-03 19:52:04 +0800

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace sfValidation
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MyObject data { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            data = new MyObject();
+            DataContext = this;
+        }
+    }
+}
---
+++ MyObject.cs
@@ -0,0 +1,48 @@
+﻿namespace sfValidation
+{
+    using SoftFluent.Windows;
+    using System;
+    using System.Collections;
+    using System.ComponentModel;
+    using System.Linq;
+
+    public class MyObject : INotifyDataErrorInfo, IDataErrorInfo, INotifyPropertyChanged
+    {
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            if (propertyName == null || propertyName == nameof(Text))
+            {
+                if (string.IsNullOrWhiteSpace(_text))
+                    yield return "Text must not be empty.";
+            }
+        }
+
+        public bool HasErrors => (GetErrors(null)?.Cast<object>()?.Any()).GetValueOrDefault();
+
+        private string _text;
+        [PropertyGridOptions(EditorDataTemplateResourceKey = "TextEditor")]
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (_text == value)
+                    return;
+
+                _text = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Text)));
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HasErrors)));
+                if ((GetErrors(nameof(Text))?.Cast<object>()?.Any()).GetValueOrDefault())
+                {
+                    ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(Text)));
+                }
+            }
+        }
+
+        string IDataErrorInfo.Error => ((IDataErrorInfo)this)[null];
+        string IDataErrorInfo.this[string columnName] => GetErrors(columnName)?.Cast<object>()?.FirstOrDefault()?.ToString();
+    }
+}
